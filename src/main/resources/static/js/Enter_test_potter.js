7d2b62dfdda49455d6c88682719e0dc5005d30f2
minjelee <123dkan@naver.com>
2022-10-12 12:08:09 +0900

    [lmj] 해리포터 테스트 작업중/ ignore에 커밋하면 안되는 파일 넣어둠

---
+++ src/main/resources/static/js/Enter_test_potter.js
@@ -0,0 +1,248 @@
+var Enter_tripnum = 1; // 문제 번호 변수
+var Enter_trip_q = {
+    1: {
+        "title": "Q1. <br/><br/>같은 기숙사 친구인 찰스가 곤경에 빠졌습니다. 당신은 어떻게 행동하실 건가요?",
+        "type": "EI",
+        "Enter_trip_test_A": "원인 제거를 위해 행동한다.",
+        "Enter_trip_test_B": "곤경에 빠지게된 이유를 물어보고 같이 해결한다.",
+        "EntertripQnum": "1/12"
+    },
+    2: {
+        "title": "Q2. <br/><br/>당신이 좋아하는 색상은 무엇인가요?",
+        "type": "EI",
+        "Enter_trip_test_A": "초록색 또는 파란색",
+        "Enter_trip_test_B": "붉은색 또는 황금색",
+        "EntertripQnum": "2/12"
+    },
+    3: {
+        "title": "Q3. <br/><br/>당신이 가장 자신있어 하는 것은?",
+        "type": "EI",
+        "Enter_trip_test_A": "용기와 친절",
+        "Enter_trip_test_B": "야망과 창의성",
+        "EntertripQnum": "3/12"
+    },
+    4: {
+        "title": "Q4. <br/><br/>좋아하는 요소를 고르시오.",
+        "type": "SN",
+        "Enter_trip_test_A": "불 또는 공기",
+        "Enter_trip_test_B": "물 또는 흙",
+        "EntertripQnum": "4/12"
+    },
+    5: {
+        "title": "Q5. <br/><br/>당신을 유혹하는 친구는 누구인가요?",
+        "type": "SN",
+        "Enter_trip_test_A": "나를 유혹할 수 있는 사람은 오로지 내 자신인걸",
+        "Enter_trip_test_B": "시리우스 블랙",
+        "EntertripQnum": "5/12"
+    },
+    6: {
+        "title": "Q6. <br/><br/>듣고 싶은 수업은 무엇인가요?",
+        "type": "SN",
+        "Enter_trip_test_A": "마법약 수업",
+        "Enter_trip_test_B": "점술 수업",
+        "EntertripQnum": "6/12"
+    },
+    7: {
+        "title": "Q7. <br/><br/>반려 동물이 당신에게 날아왔습니다!!<br/>이때 당신의 반려 동물의 종류는 무엇인가요?",
+        "type": "TF",
+        "Enter_trip_test_A": "부엉~부엉~부엉이~노래를 한다~",
+        "Enter_trip_test_B": "벰! (뱀의 언어를 사용할 수 있는 해리포터님 존경스럽습니다.)",
+        "EntertripQnum": "7/12"
+    },
+    8: {
+        "title": "Q8. <br/><br/>퀴디치 선발 선수로 뽑힌 당신!<br/>포지션은 무엇일까요?",
+        "type": "TF",
+        "Enter_trip_test_A": "정렬적인 몰이꾼 또는 얍삽한 파수꾼",
+        "Enter_trip_test_B": "강력한 추격꾼 또는 진짜 빠른 수색군",
+        "EntertripQnum": "8/12"
+    },
+    9: {
+        "title": "Q9. <br/><br/>현재 나이 11살로 입학하셨습니다.<br/>당신의 학년은?",
+        "type": "TF",
+        "Enter_trip_test_A": "6학년 - 난 천재야", /*양심챙겨*/
+        "Enter_trip_test_B": "1학년 - 정석대로 간다.",
+        "EntertripQnum": "9/12"
+    },
+    10: {
+        "title": "Q10. <br/><br/>시험까지 일주일이 남았습니다!<br/>당신은 지금 무얼하고 계신가요?",
+        "type": "JP",
+        "Enter_trip_test_A": "시험에 대해 생각해본다.",
+        "Enter_trip_test_B": "공부를 시작한다.",
+        "EntertripQnum": "10/12"
+    },
+    11: {
+        "title": "Q11. <br/><br/>당신의 핸드폰 기종은 무엇인가요?",
+        "type": "JP",
+        "Enter_trip_test_A": "아이폰 14 pro",
+        "Enter_trip_test_B": "갤럭시 22 ultra",
+        "EntertripQnum": "11/12"
+    },
+    12: {
+        "title": "Q12. <br/><br/>주로 선호하는 교복 스타일은 어떤건가요?",
+        "type": "JP",
+        "Enter_trip_test_A": "단정한 교복",
+        "Enter_trip_test_B": "편안한 교복",
+        "EntertripQnum": "12/12"
+    }
+}
+var result = {
+    "ISTJ": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_gyeongju(ISTJ).jpeg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "슬린데린",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "ISTP": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_Ulleung-gun(ISTP).jpeg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "레번클로",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "ISFJ": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_Jeonju(ISFJ).png",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "슬린데린",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "ISFP": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_danyang(ISFP).jpeg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "후풀푸프",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "INTJ": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_sacheon(INTJ).jpeg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "레번클로",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "INTP": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_damyang(INTP).jpeg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "레번클로",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "INFJ": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_wanju(INFJ).jpeg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "레번클로",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "INFP": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_Daewallyeong(INFP).jpeg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "후플푸프",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "ESTJ": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_Seoul(ESTJ).jpeg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "슬린데린",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "ESTP": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_Jeju(ESTP).jpeg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "그린핀도르",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "ESFJ": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_Gangneung1(ESFJ).jpg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "후플푸프",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "ESFP": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_gangneung(ESFP).png",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "그린핀도르 ",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "ENTJ": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_dangjin(ENTJ).jpeg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "슬린데린",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "ENTP": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_busan(ENTP).jpeg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "그린핀도르",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "ENFJ": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_sokcho(ENFJ).jpeg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "후플푸프",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    },
+    "ENFP": {
+        "img": "/img/Enter_trip_test/Enter_trip_test_yangyang(ENFP).jpeg",
+        "Enter_trip_keyword": "당신의 기숙사는",
+        "Enter_trip": "그린핀도르",
+        "explain_title": "생각중",
+        "explain": "귀찮다",
+    }
+}
+
+function Enter_start() {
+    $(".trip_start").hide();
+    $(".trip_question").show();
+    next();
+}
+
+$("#Enter_trip_test_A").click(function () {
+    var type = $("#type").val();
+    var preValue = $("#" + type).val();
+    $("#" + type).val(parseInt(preValue) + 1);
+    next();
+});
+$("#Enter_trip_test_B").click(function () {
+    next();
+});
+
+function next() {
+    if (Enter_tripnum == 13) {
+        $(".trip_question").hide();
+        $(".result").show();
+
+        var Enter_mbti_trip = "";
+        ($("#EI").val() < 2) ? Enter_mbti_trip += "I" : Enter_mbti_trip += "E";
+        ($("#SN").val() < 2) ? Enter_mbti_trip += "N" : Enter_mbti_trip += "S";
+        ($("#TF").val() < 2) ? Enter_mbti_trip += "F" : Enter_mbti_trip += "T";
+        ($("#JP").val() < 2) ? Enter_mbti_trip += "P" : Enter_mbti_trip += "J";
+        alert("결과 확인하기");
+
+        $("#img").attr("src", result[Enter_mbti_trip]["img"]);
+        $("#Enter_trip").html(result[Enter_mbti_trip]["Enter_trip"]);
+        $("#Enter_trip_keyword").html(result[Enter_mbti_trip]["Enter_trip_keyword"]);
+        $("#explain_title").html(result[Enter_mbti_trip]["explain_title"]);
+        $("#explain").html(result[Enter_mbti_trip]["explain"]);
+        $("#explain_tourist").html(result[Enter_mbti_trip]["explain_tourist"]);
+
+    } else {
+        $(".progress-bar").attr('style', 'width: calc(100/12*' + Enter_tripnum + '%)');
+        $("#trip_title").html(Enter_trip_q[Enter_tripnum]["title"]);
+        $("#EntertripQnum").html(Enter_trip_q[Enter_tripnum]["EntertripQnum"]);
+        $("#type").val(Enter_trip_q[Enter_tripnum]["type"]);
+        $("#Enter_trip_test_A").html(Enter_trip_q[Enter_tripnum]["Enter_trip_test_A"]);
+        $("#Enter_trip_test_B").html(Enter_trip_q[Enter_tripnum]["Enter_trip_test_B"]);
+        Enter_tripnum++;
+    }
+
+}
---
+++ src/main/webapp/Entertainment/Enter_test_potter.jsp
@@ -0,0 +1,98 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>해리포터 테스트</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> <!-- 부트스트랩 -->
+    <script type="text/javascript" src="/js/jquery-3.6.0.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <script type="text/javascript" src="/js/includeHtml.js"></script>
+    <link rel="stylesheet" type="text/css" href="/css/common.css"> <!-- 공통 css -->
+    <link rel="stylesheet" type="text/css" href="/css/Entertainment/Enter_test_potter.css">
+</head>
+
+<body>
+<jsp:include page="../fixed/header.jsp"></jsp:include>
+<div class="col-12 potter">
+    <!-- 시작 화면 -->
+    <article class="col-12 mb-4 trip_start">
+        <div class="mb-5 title">너나들이</div>
+        <%--Enter_trip_test_title--%>
+        <div class="mb-5 col-12 col-lg-10" style="margin-left: auto; margin-right: auto;">
+            <img class="mb-5 Enter_image" src="/img/Enter_trip_test/Enter_trip_test_title.jpg">
+            <h1 class="mb-4 text-center">헤리포터 테스트</h1>
+            <h5 class="mb-5 text-center">"Sorting hat"가 선택해주는 당신의 기숙사를 알아보러 가보자고</h5>
+            <%-- 버튼 지정해서 css 입히기--%>
+            <div class="col-12 mb-5 text-center">
+                <button type="button" class="btn btn-outline-secondary col-12 mb-5" style="padding-top:25px; padding-bottom:25px; font-size: large;" onclick="Enter_start();">테스트 시작하기</button>
+                <button type="button" class="btn btn-outline-secondary col-12" style="padding-top:25px; padding-bottom:25px; font-size: large;" onclick="location.href='/Entertainment/Entertainment.jsp'">다른 테스트 보러가기</button>
+            </div>
+            <h5 class="mb-2 text-center">테스트 공유하기</h5>
+            <!-- Go to www.addthis.com/dashboard to customize your tools -->
+            <div class="mb-2 text-center share addthis_inline_share_toolbox"></div>
+        </div>
+    </article>
+
+    <!-- 문제 화면 -->
+    <article class="col-12 mb-4 trip_question">
+        <div class="mb-2 title">해리포터 테스트</div>
+        <div class="col-12 col-lg-10" style="margin-left: auto; margin-right: auto;">
+            <h6 id="EntertripQnum" class="mb-2" style="text-align:right; color: rgb(137, 137, 137);">문제 번호</h6>
+            <div class="mb-2 progress">
+                <div class="mb-10 progress-bar bg-success" role="progressbar" aria-label="Basic example" style="width: calc(100/12*1%)"></div>
+            </div>
+            <div class="col-12" id="trip_title">문제</div>
+            <!--<h2 id="trip_title" class="text-center mt-10">문제</h2>-->
+            <input id="type" type="hidden" value="EI">
+            <div class="col-12 text-center" style="display: grid;">
+                <button id="Enter_trip_test_A" type="button" class="btn btn-light mb-4" style="padding: 28px 15px; border-radius: 12px;">Primary</button>
+                <button id="Enter_trip_test_B" type="button" class="btn btn-light" style="padding: 28px 15px; border-radius: 12px;">Primary</button>
+            </div>
+        </div>
+    </article>
+
+    <!-- 결과 화면 -->
+    <article class="col-12 mb-4 result">
+        <div class="mb-5 title">해리포터 테스트</div>
+        <div class="col-12 col-lg-10" style="margin-left: auto; margin-right: auto;">
+            <h1 id="Enter_trip_keyword" class="text-center mb-4">기숙사 키워드</h1>
+            <h2 id="Enter_trip" class="text-center mb-4">기숙사</h2>
+            <div class="col-12 text-center">
+                <img id="img" src="" class="rounded-circle mb-4" alt="Enter_trip">
+            </div>
+            <div id="Enter_trip_result" class="mb-3" style="border: 1px solid #ddd;">
+                <h3 id="explain_title" class="mb-1">타이틀 설명</h3>
+                <h3 id="explain" class="mb-1">여행 스타일 설명</h3>
+            </div>
+            <!-- Go to www.addthis.com/dashboard to customize your tools -->
+            <div class="mb-5 share addthis_inline_share_toolbox text-center"></div>
+            <button id="Enter_page" type="button" class="btn btn-light text-center mb-5" style="height: 60px; margin-bottom: 30px;" onclick="location.href='/Entertainment/Entertainment.jsp'">다른 테스트 하러 가기!</button>
+        </div>
+    </article>
+
+    <!-- 타입별로 데이터 저장 -->
+    <input type="hidden" id="EI" value="0">
+    <input type="hidden" id="SN" value="0">
+    <input type="hidden" id="TF" value="0">
+    <input type="hidden" id="JP" value="0">
+</div>
+<jsp:include page="../fixed/footer.jsp"></jsp:include>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script> <!-- jquery -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+        crossorigin="anonymous"></script>  <!-- 스크립트 -->
+<!-- addthis code / sns 공유 기능 -->
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-62f6d80e58d7a901"></script>
+<script type="text/javascript" src="/js/Enter_test_potter.js"></script>
+</body>
+</html>
