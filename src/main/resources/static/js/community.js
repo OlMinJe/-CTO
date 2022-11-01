a6e39d484acd7a3b51e173061b2a1192803f9967
minjelee <123dkan@naver.com>
2022-11-01 20:48:43 +0900

    [lmj] 커뮤니티 코드 정리

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -4,15 +4,14 @@ let active_editor = document.querySelector("#community_editor");
 
 /* 하위 메뉴 - 커뮤니티 */
 let normal = document.getElementById('sub-menu');
-//let normal_li = document.querySelectorAll('.menu-box-02 li');
 
 /* content */
 let table = document.getElementById('community-table');
 let editor_content = document.getElementById('editor_content');
 
 /* 커뮤니티 하위 메뉴를 클릭했을 때의 css 이벤트 */
-let div2 = document.getElementsByClassName("div2");//$(".div2")
-let clickMenu = [];
+let div2 = document.getElementsByClassName("div2");
+
 // 모바일 버전
 function sub_menu() {
     // 반응형 하위 메뉴 - 화면 크기가 767px 이하일 경우
@@ -24,18 +23,16 @@ function sub_menu() {
         }
     } else { normal.style.display = "block"; }
 }
+
 function menuClick() {
     var sch = location.search;
     var params = new URLSearchParams(sch);
 
     for (var i = 0; i < div2.length; i++) {
         if(div2[i].classList[1] == "active"){
-            console.log('active 삭제됨');
             div2[i].classList.remove("active");
         }
     }
-
-    console.log(params.get('category'));
 
     if(params.get('category') == 10){
         div2[0].classList.add("active");
@@ -55,30 +52,12 @@ function li_active_01() {
     // 초기에 출력되는 content 영역
     table.style.display = "table";
     editor_content.style.display = "none";
-    //dataRedundancy();
 
     /* 상위 메뉴 class */
     active_normal.classList.add('active');
     active_editor.classList.remove('active');
-
-    // 하위 메뉴 영역
- /*  for(let i = 0; i < normal_li.length; i++) {
-       normal_li[i].addEventListener("click", function () {
-            선택한 메뉴에 active 클래스 추가 및 이전 메뉴 active 클래스 삭제
-            for(let j = 0; j < normal_li.length; j++) {
-               if(normal_li[i].classList.contains('active') == true) {
-                   normal_li[i].classList.remove('active');
-               } else {
-                   normal_li[i].classList.add('active');
-               }
-
-            }
-            dataRedundancy();
-       })
-    }*/
     sub_menu();
 }
-
 li_active_01();
 
 /* 에디터 탭을 클릭했을 떄의 이벤트 */
@@ -93,15 +72,3 @@ function li_active_02(){
     table.style.display = "none";
     editor_content.style.display = "block";
 }
-/*
-active_editor.addEventListener("click", function () {
-    /!* 상위 메뉴 class *!/
-    active_normal.classList.remove('active');
-    active_editor.classList.add('active');
-    /!* 하위 메뉴 class *!/
-    normal.style.display = "none";
-    /!* 데이터 중복 현상 해결*!/
-    //dataRedundancy();
-    editor_content.style.display = "block";
-    table.style.display = "none";
-})*/
